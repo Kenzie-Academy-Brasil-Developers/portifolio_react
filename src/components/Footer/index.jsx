13af99eefd17cf572f0c00d57dc471d99e764e65
Lucas Dias <lucasalvesdiasdasilva123@gmail.com>
2023-09-27 22:46:33 -0300

    styles finished

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,17 +1,22 @@
 import whatsappLogo from "../../assets/whatsapp-icon.png";
 import linkedin from "../../assets/linkedin-icon.png";
 import gitHub from "../../assets/github-icon.png";
+import styles from "./style.module.css";
 
 export const Footer = ({ user }) => {
   return (
-    <div className="container">
-      <h2 className="title-2">Contato</h2>
-      <div>
-        <img src={whatsappLogo} alt={"whatsapp"} />
-        <img src={linkedin} alt={"linkedin"} />
-        <img src={gitHub} alt={"gitHub"} />
+    <footer className={`container ${styles.footerContainer}`}>
+      <div className={styles.flexDiv}>
+        <div className={styles.flexContactsAndCopyright}> 
+          <h2 className="title-2">Contato</h2>
+          <div className={styles.flexDivImg}>
+            <img src={whatsappLogo} alt={"whatsapp"} />
+            <img src={linkedin} alt={"linkedin"} />
+            <img src={gitHub} alt={"gitHub"} />
+          </div>
+        </div>
+        <small className="paragraph">{user}</small>
       </div>
-      <small>{user}</small>
-    </div>
+    </footer>
   );
 };
--- src/components/ProjectsSection/ProjectsCard/index.jsx
+++ src/components/ProjectsSection/ProjectsCard/index.jsx
@@ -1,12 +1,14 @@
+import styles from "./style.module.css"
+
 export const ProjectCard = ({ img, title, name, description, link }) => {
   return (
-    <li>
-      <div>
+    <li className={styles.flexLi}>
+      <div className={styles.flexDiv}>
         <h3 className="title-3">{title}</h3>
         <img src={img} alt={name} />
       </div>
       <p className="text">{description}</p>
-      <a href="#">{link}</a>
+      <a className={`link ${styles.linkAlt}`} href="#">{link}</a>
     </li>
   );
 };
